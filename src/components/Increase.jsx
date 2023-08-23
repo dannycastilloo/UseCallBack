44b8ac3812afab7c6801878da19283078bdb2e27
dannycastilloo <dannycastillootiniano@gmail.com>
2023-08-22 23:44:35 -0500

    update

--- src/components/Increase.jsx
+++ src/components/Increase.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export const Increase = React.memo(({ increase }) => {
 
-    console.log('Me estoy redibujando')
-
     return (
         <div className='action'>
             <button className="btn btn-danger" onClick={() => increase(10)}>+1</button>
